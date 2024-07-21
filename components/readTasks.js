677788b23080ac0e270a8be3fb8489f0da379a23
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-07-20 18:20:27 -0600

    feat: organizing by date
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,13 +1,22 @@
 import { createTask } from "./addTask.js";
+import { uniqueDates } from "../services/date.js";
 import dateElement from "./dateElement.js";
 
-export const readTasks = () => {
+export const displayTasks = () => {
   const list = document.querySelector("[data-list]");
-  console.log(list);
-
+  
   const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-  taskList.forEach((task) => {
-    list.appendChild(dateElement(task.dateFormat));
-    list.appendChild(createTask(task));
+  const dates = uniqueDates(taskList);
+
+  dates.forEach((date) => {
+    const dateMoment = moment(date, "DD/MM/YYYY");
+    list.appendChild(dateElement(date));
+    taskList.forEach((task) => {
+      const taskDate = moment(task.dateFormat, "DD/MM/YYYY");
+      const diff = dateMoment.diff(taskDate);
+      if (diff === 0) {
+        list.appendChild(createTask(task));
+      }
+    });
   });
 };
